8da1bc557cbfce8a3965ba39df35e95521af9027
Gunjika <gunjikavrshn23@gmail.com>
2020-07-19 13:51:58 +0530

    logger added

--- BootcampProject/src/main/java/com/example/demo/BootcampProjectApplication.java
+++ BootcampProject/src/main/java/com/example/demo/BootcampProjectApplication.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampProjectApplication {
+	private static final Logger logger = LoggerFactory.getLogger(BootcampProjectApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+	      logger.warn("this is a warn message");
+	      logger.error("this is a error message");
 		SpringApplication.run(BootcampProjectApplication.class, args);
 	}
 
